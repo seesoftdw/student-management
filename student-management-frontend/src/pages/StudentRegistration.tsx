5d79b64ce092b4efa266b0324fa74a971cce58fa
MuskanSharma1923 <muskansharma6972@gmail.com>
2025-04-04 14:43:47 +0530

    correcting file extensions

---
+++ student-management-frontend/src/pages/StudentRegistration.tsx
@@ -0,0 +1,18 @@
+// import React from 'react'
+
+// interface Student{
+//   name: string;
+//   gender: string;
+//   age: number;
+//   email: string;
+//   contact: string;
+//   address: string;
+// }
+
+// const StudentRegistration = () => {
+//   return (
+//     <div></div>
+//   )
+// }
+
+// export default StudentRegistration
